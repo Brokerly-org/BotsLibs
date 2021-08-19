307a478f07a28f0eb3ef263928137e9ed1e49156
hvuhsg <34660046+hvuhsg@users.noreply.github.com>
2021-08-19 15:01:28 +0300

    Update echo_bot.py

--- PyBrokerly/examples/echo_bot.py
+++ PyBrokerly/examples/echo_bot.py
@@ -1,10 +1,10 @@
 from brokerly import Bot
 
 def handler(context, update):
-    chat_id = update.chat
-    message = update.text
-    print(f'New message from chat {chat_id}: {message}')
-    context.send_message(chat_id, message)
+    chat_id = update.message["chat_id"]
+    text = update.message["content"]
+    print(f'New message from chat {chat_id}: {text}')
+    context.send_message(chat_id, text)
 
 bot = Bot('127.0.0.1', 80, 'nblNTu1zMWTQrte0p5KJ', handler)
 bot.start(interval = 1)
