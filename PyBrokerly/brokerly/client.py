bc647a025c288cb53f392eb948695396da70fd15
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-08-22 14:35:41 +0300

    run handler in a thread (add workers)

--- PyBrokerly/brokerly/client.py
+++ PyBrokerly/brokerly/client.py
@@ -1,9 +1,12 @@
 import requests
 import time
+from concurrent.futures.thread import ThreadPoolExecutor
+
 
 class Message:
     def __init__(self, json) -> None:
         self.data = json
+
     def __getattr__(self, name: str):
         return self.data[name]
 
@@ -13,11 +16,16 @@ class Message:
     def __getitem__(self, key):
         return self.data[key]
 
+
 class Bot:
-    def __init__(self, host, port, token, message_handler) -> None:
+    def __init__(self, token: str, message_handler, host: str, port: int = None, workers: int = 4) -> None:
         self.token = token
-        self.server_url = f'http://{host}:{port}'
+        if port is not None:
+            self.server_url = f'http://{host}:{port}'
+        else:
+            self.server_url = f'http://{host}'
         self.handler = message_handler
+        self.executor = ThreadPoolExecutor(max_workers=workers)
             
     def get_updates(self):
         response = requests.get(f"{self.server_url}/bot/pull", params={"token": self.token})
@@ -29,13 +37,12 @@ class Bot:
             params={"token": self.token, "chat_id": chat_id, "message": message}
         )
 
-    def start(self, interval = 5):
+    def start(self, interval=5):
         while True:
             update = self.get_updates()
             chats = update['chats']
             for chat in chats:
                 for message in chat['messages']:
                     message = Message({"message": message})
-                    
-                    self.handler(self, message)
+                    self.executor.submit(self.handler, (message,))
             time.sleep(interval)            
--- PyBrokerly/examples/echo_bot.py
+++ PyBrokerly/examples/echo_bot.py
@@ -1,10 +1,11 @@
 from brokerly import Bot
 
+
 def handler(context, update):
     chat_id = update.message["chat_id"]
     text = update.message["content"]
     print(f'New message from chat {chat_id}: {text}')
     context.send_message(chat_id, text)
 
-bot = Bot('127.0.0.1', 80, 'nblNTu1zMWTQrte0p5KJ', handler)
-bot.start(interval = 1)
+bot = Bot(token='nblNTu1zMWTQrte0p5KJ', message_handler=handler, host='127.0.0.1', port=80)
+bot.start(interval=1)
